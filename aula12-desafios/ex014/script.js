a0305e76172883d8ba0ac8e37b008f2c148ddb05
Arthur Gomes <101721044+arthurgomes1k@users.noreply.github.com>
2024-01-07 23:51:26 -0300

    desafio 15 aula12 pronto

--- aula12-desafios/ex014/script.js
+++ aula12-desafios/ex014/script.js
@@ -5,14 +5,19 @@ function carregar() {
     var hora = data.getHours()
     msg.innerHTML = `Agora são ${hora} horas.`
 
-    if (hora >= 0 && hora < 12) {
+    if (hora >= 5 && hora < 12) {
         img.src = 'imagens/manha.png'
         document.body.style.background = '#e2cd9f'
+        // Bom Dia!
+
     } else if (hora >= 12 && hora < 18) {
         img.src = 'imagens/tarde.png'
         document.body.style.background = '#b9846f'
+        // Boa Tarde!
+
     } else {
         img.src = 'imagens/noite.png'
         document.body.style.background = '#515154'
+        // Boa Noite!
     }
 }
