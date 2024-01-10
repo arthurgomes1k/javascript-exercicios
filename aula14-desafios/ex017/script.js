811ad1eed052979a36c9ab0b4663a35bac96e939
Arthur Gomes <101721044+arthurgomes1k@users.noreply.github.com>
2024-01-09 23:08:59 -0300

    aula14 Repetições - desafio 2

--- aula14-desafios/ex017/script.js
+++ aula14-desafios/ex017/script.js
@@ -1,1 +1,25 @@
-alert('Olá!')
+function tabuada() {
+    let num = document.getElementById('num')
+    let tab = document.getElementById('seltab')
+
+    if (num.value.length == 0) {
+        window.alert('Por favor, digite um número!')
+    } else {
+        let n = Number(num.value)
+        let c = 1
+        tab.innerHTML = ''
+        while (c <= 10) {
+            let item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n * c}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
+            c++
+        }
+    }
+
+    /*
+    for (let index = 0; index < 10; index++) {
+        res.innerHTML += `${n} x ${index} = ${n * index} <br>`
+    }
+    */
+}
